79bf2a39d396b406649e6519d3e66894371ea4ec
Devon <djohnjr22@outlook.com>
2024-11-19 18:23:15 -0500

    java and rust solution

--- merge_sort_java/MergeSort.java
+++ merge_sort_java/MergeSort.java
@@ -7,14 +7,14 @@ public class MergeSort {
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void mergeSort(int[] arr_alt) {
-        if (arr_alt.length > 1) {
-            int mid = arr_alt.length / 2;
+    public static void mergeSort(int[] arr) {
+        if (arr.length > 1) {
+            int mid = arr.length / 2;
             int[] left = new int[mid];
-            int[] right = new int[arr_alt.length - mid];
+            int[] right = new int[arr.length - mid];
 
-            System.arraycopy(arr_alt, 0, left, 0, mid);
-            System.arraycopy(arr_alt, mid, right, 0, arr_alt.length - mid);
+            System.arraycopy(arr, 0, left, 0, mid);
+            System.arraycopy(arr, mid, right, 0, arr.length - mid);
 
             mergeSort(left);
             mergeSort(right);
@@ -23,23 +23,23 @@ public class MergeSort {
 
             while (i < left.length && j < right.length) {
                 if (left[i] < right[j]) {
-                    arr_alt[k] = left[i];
+                    arr[k] = left[i];
                     i += 1;
                 } else {
-                    arr_alt[k] = right[j];
+                    arr[k] = right[j];
                     j += 1;
                 }
                 k += 1;
             }
 
             while (i < left.length) {
-                arr_alt[k] = left[i];
+                arr[k] = left[i];
                 i += 1;
                 k += 1;
             }
 
             while (j < right.length) {
-                arr_alt[k] = right[j];
+                arr[k] = right[j];
                 j += 1;
                 k += 1;
             }
